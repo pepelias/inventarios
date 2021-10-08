2806229745a21fd602600185632707b543d34cfe
pepelias <j.avellogomez@gmail.com>
2021-10-08 01:28:41 -0300

    Con UPC E

--- src/pages/CapturePage.js
+++ src/pages/CapturePage.js
@@ -16,7 +16,7 @@ export const CapturePage = () => {
           target: viewport.current, // Or '#yourElement' (optional)
         },
         decoder: {
-          readers: ['code_128_reader'],
+          readers: ['upc_e_reader'],
         },
       },
       function (err) {
@@ -24,7 +24,7 @@ export const CapturePage = () => {
           console.log(err)
           return
         }
-        alert('Leeremos code_128_reader')
+        alert('Leeremos upc')
         console.log('Initialization finished. Ready to start')
         Quagga.start()
       }
